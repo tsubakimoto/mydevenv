c546ff6ae04c6746d6c63e4ef91e6bb1bfb96c08
Yuta Matsumura <tkmbmbr@gmail.com>
2014-07-18 11:50:46 +0900

    add bitnami redmine files

---
+++ vagrant_resources/bootstrap2.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+
+yum update -y
+
+# iptables
+iptables -F
+service iptables stop
+chkconfig iptables off
+
+# optional
+yum install -y vim
+
+# install require packages for git
+yum install -y git
+
+# download bitnami redmine
+wget https://bitnami.com/redirect/to/38030/bitnami-redmine-2.5.2-0-linux-x64-installer.run /home/vagrant/
+
+# Copy config files
+cp -f /vagrant/vagrant_resources/selinux_config /etc/selinux/config
+
+echo 'complete provisioning!'
